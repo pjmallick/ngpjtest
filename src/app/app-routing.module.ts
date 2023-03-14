0de6f77b519eb8d9ed31f8f8d14287fdf87b6508
pmall <pmallick@outlook.com>
2023-03-14 10:19:01 -0400

    Add ng project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GradeComponent } from './grade/grade.component';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'student', component: StudentComponent },
+  { path: 'grade', component: GradeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GradeComponent } from './grade/grade.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,6 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, StudentComponent, GradeComponent]
 })
 export class AppModule { }
